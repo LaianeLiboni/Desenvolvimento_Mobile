3aad7bfb80f1c504399023d2795cef02a77ffb04
LaianeLiboni <69061205+LaianeLiboni@users.noreply.github.com>
2021-08-16 15:48:16 -0300

    Add files via upload

---
+++ src/main/java/com/example/appcofre/credencialModel.java
@@ -0,0 +1,33 @@
+package com.example.appcofre;
+
+public class credencialModel {
+
+    private int id;
+    private String nome;
+    private String Usuario;
+    private String Senha;
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
+}
